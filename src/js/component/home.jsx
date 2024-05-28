af9e82cbad80d2d31c8cb367933da8e7a318d600
matii17 <154836261+matii17@users.noreply.github.com>
2024-05-28 05:01:35 +0000

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,65 @@
-import React from "react";
+import React, {useState,useEffect}from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+
+    const[selected, setSelected]= useState("red")
+    const[active,setActive]= useState(false)
+
+const changeColor = (color) =>{
+    setSelected(color)
+}
+
+useEffect(() =>{
+if(active) {
+const interval = setInterval(()=>{
+if(selected == "red"){
+    setSelected("yellow")
+}else if(selected == "yellow"){
+    setSelected("green")
+}else if(selected == "green"){
+    setSelected("red")
+}
+},1000)
+return () => clearInterval(interval)
+}
+
+
+
+
+
+
+
+
+},[active,selected])
+
+
+    return (
+        <main>
+
+            <div className="p-trafficlight"></div>
+            <div className="traffic-light">
+             <div 
+             className={`red ${selected == "red" ? "light-on" : ""}`}
+             onClick={() => changeColor("red")}
+             ></div>
+             <div 
+             className={`yellow ${selected == "yellow" ? "light-on" : ""}`}
+             onClick={() => changeColor("yellow")}
+             ></div>
+             <div 
+             className={`green ${selected == "green" ? "light-on" : ""}`}
+             onClick={() => changeColor("green")}
+             ></div>
+
+            </div>
+            <button onClick={()=>setActive(!active)}className="btn btn-success">Activar</button>
+        </main>
+    );
 };
 
 export default Home;
+
